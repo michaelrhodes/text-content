57d87e5ff4373d49f38a562bfca33c86dc4ba110
Julian Gruber <julian@juliangruber.com>
2014-09-08 16:34:59 +0100

    add failing clear test

--- test/index.js
+++ test/index.js
@@ -1,12 +1,22 @@
 var run = require('tape')
 var text = require('../')
 
-var span = document.createElement('span')
-var value = 'npm.im/text-content'
-
 run('it works', function(test) {
+  var span = document.createElement('span')
+  var value = 'npm.im/text-content'
+
   test.notOk(text(span), 'has no value')
   text(span, value)
   test.equal(text(span), value, 'set value found')
+  test.end()
+})
+
+run('it clears', function(test) {
+  var span = document.createElement('span')
+
+  text(span, 'hi')
+  test.equal(text(span), 'hi', 'value found')
+  text(span, null)
+  test.notOk(text(span), 'has no value')
   test.end()
 })
